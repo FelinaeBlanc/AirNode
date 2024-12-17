ffe533bd3b6326cdb39b178f90336ebf66c761ef
FelinaeBlanc <115179383+FelinaeBlanc@users.noreply.github.com>
2024-12-17 11:53:41 +0100

    Création du manuel

--- web/script_start/script.sh
+++ web/script_start/script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Se déplacer dans le répertoire du script
 cd "$(dirname "$0")"
 
 # Activer l'environnement virtuel
 source venv/bin/activate
 
-# Exécuter les scripts Python avec leurs chemins relatifs
-python3 xbee_module/xbee_receiver.py
-python3 web/python_web/app.py
+# Exécuter les scripts Python avec sudo en arrière-plan
+sudo python3 xbee_module/xbee_receiver.py &
+sudo python3 web/python_web/app.py &
